f2430681d2ca5e11c51c678a34a70b32719fca68
Evyweb <dev@evyweb.fr>
2023-01-18 12:18:20 +0100

    feat(chore): Add basic modules and folders structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AuthGuard} from "./auth/utils/auth.guard";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'games'},
+  {path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
+  {path: 'games', loadChildren: () => import('./games/games.module').then(m => m.GamesModule), canActivate: [AuthGuard]},
+  {path: 'streams', loadChildren: () => import('./streams/streams.module').then(m => m.StreamsModule), canActivate: [AuthGuard]},
+  {path: '**', pathMatch: 'full', redirectTo: 'games'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/streams/streams.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { StreamsRoutingModule } from './streams-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    StreamsRoutingModule
+  ]
+})
+export class StreamsModule { }
