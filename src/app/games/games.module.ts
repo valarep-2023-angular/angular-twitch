f9a4ae6a06beb532e09497d547f4f445db13dfe3
BeNyEz17 <benjaminpluchart10@gmail.com>
2023-01-18 16:38:18 +0100

    feat(game): add gamecomponent for ui

--- src/app/games/games.module.ts
+++ src/app/games/games.module.ts
@@ -2,10 +2,12 @@ import { NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GamesRoutingModule } from './games-routing.module';
 import { TopGamesComponent } from './feature/top-games/top-games.component';
+import { GameComponent } from './ui/game/game.component';
 
 @NgModule({
   declarations: [
-    TopGamesComponent
+    TopGamesComponent,
+    GameComponent
   ],
   imports: [
     CommonModule,
