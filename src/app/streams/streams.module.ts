9d62d60b5adc944686c574cfcf89c90134c3cd11
EllVodka <eric.sergueev@gmail.com>
2023-02-12 21:24:28 +0100

    refactor(get-streams-by-game-id): add stream-card

--- src/app/streams/streams.module.ts
+++ src/app/streams/streams.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { StreamsRoutingModule } from './streams-routing.module';
 import { GetStreamsByGameIdComponent } from './feature/get-streams-by-game-id/get-streams-by-game-id.component';
 import { ResizePipe } from './feature/get-streams-by-game-id/change-width-height.pipe';
+import { StreamCardComponent } from './ui/stream-card/stream-card.component';
 
 
 @NgModule({
   declarations: [
     GetStreamsByGameIdComponent,
-    ResizePipe
+    ResizePipe,
+    StreamCardComponent
   ],
   imports: [
     CommonModule,
