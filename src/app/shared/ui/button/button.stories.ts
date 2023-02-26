aab6cd908f7cde753de5655999aafb5006853660
Evyweb <dev@evyweb.fr>
2023-02-26 23:56:32 +0100

    feat(storybook): Add storybook to the project

---
+++ src/app/shared/ui/button/button.stories.ts
@@ -0,0 +1,26 @@
+import { Meta, moduleMetadata, Story } from '@storybook/angular';
+
+
+import { ButtonComponent } from './button.component';
+
+export default {
+  title: 'Buttons',
+  component: ButtonComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [],
+    })
+  ],
+  args: {
+    text: 'Button',
+  }
+} as Meta<ButtonComponent>;
+
+const Template: Story = (args) => ({
+  props: args,
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+  text: 'Primary',
+}
