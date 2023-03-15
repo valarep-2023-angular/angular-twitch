209d6bd9cc8094ce2e8fa2268952068241f645cf
EllVodka <eric.sergueev@gmail.com>
2023-03-15 16:53:07 +0100

    fix(load-player-stream): add switchMap

--- src/app/streamer/feature/load-player-stream/load-player-stream.component.ts
+++ src/app/streamer/feature/load-player-stream/load-player-stream.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { environment } from 'src/environments/environment';
-import {map, mergeMap} from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { map, mergeMap } from 'rxjs/operators';
+import { Observable, switchMap } from 'rxjs';
 import { UserDto } from 'src/app/shared/dto/user.dto';
 import { UsersService } from 'src/app/shared/data-access/users/users.service';
 import { StreamService } from 'src/app/shared/data-access/streams/stream.service';
@@ -17,14 +17,19 @@ export class StreamPlayerComponent {
 
   urlStream!: string;
   user$?: Observable<UserDto>;
-  stream$?:Observable<StreamDto>;
+  stream$?: Observable<StreamDto>;
 
-  constructor(private route: ActivatedRoute, private userService: UsersService, private streamService:StreamService) { }
+  constructor(private route: ActivatedRoute, private userService: UsersService, private streamService: StreamService) { }
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     const login = this.route.snapshot.paramMap.get("streamerName") || "";
-    this.urlStream = environment.twitch.streamPlayerUrl+login;
-    this.user$ = this.userService.getUserByLogin$(login); 
-    this.stream$= this.streamService.getStreamByUserLogin$(login);
+    this.urlStream = environment.twitch.streamPlayerUrl + login;
+    
+    this.user$ = this.route.params.pipe(
+      switchMap(params => this.userService.getUserByLogin$(params["streamerName"]))
+    );
+    this.stream$ = this.route.params.pipe(
+      switchMap(params => this.streamService.getStreamByUserLogin$(params["streamerName"]))
+    )
   }
 }
