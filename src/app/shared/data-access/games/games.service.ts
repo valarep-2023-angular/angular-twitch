cd5448bcc0ace01f3c53dcf01a08605668dc8f22
ThibautFiquet <thibautfiquet792@gmail.com>
2023-03-29 14:04:05 +0200

    feat(categories)get-50-first-categories

--- src/app/shared/data-access/games/games.service.ts
+++ src/app/shared/data-access/games/games.service.ts
@@ -1,9 +1,9 @@
-import {map, Observable} from "rxjs";
-import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
-import {Game} from "../../dto/game";
-import {TwitchResponseDto} from "../../../api/data-access/twitch-response.dto";
-import {GameDto} from "../../dto/game.dto";
+import { map, Observable } from "rxjs";
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Game } from "../../dto/game";
+import { TwitchResponseDto } from "../../../api/data-access/twitch-response.dto";
+import { GameDto } from "../../dto/game.dto";
 
 @Injectable({
   providedIn: 'root'
@@ -17,13 +17,28 @@ export class GamesServiceService {
     return this.http.get<TwitchResponseDto<GameDto>>('/games/top').pipe(
       map(response => response.data),
       map(gameDtos => gameDtos.map(gameDto => ({
-            title: gameDto.name,
-            image: gameDto.box_art_url,
-            tags: gameDto.tags || [],
-            subTitle: gameDto.subTitle,
-            slug: gameDto.id,
-          })
-        )
+        title: gameDto.name,
+        image: gameDto.box_art_url,
+        tags: gameDto.tags || [],
+        subTitle: gameDto.subTitle,
+        slug: gameDto.id,
+      })
+      )
+      )
+    )
+  }
+
+  getCategorie(): Observable<Game[]> {
+    return this.http.get<TwitchResponseDto<GameDto>>('/games/top?first=50').pipe(
+      map(response => response.data),
+      map(gameDtos => gameDtos.map(gameDto => ({
+        title: gameDto.name,
+        image: gameDto.box_art_url,
+        tags: gameDto.tags || [],
+        subTitle: gameDto.subTitle,
+        slug: gameDto.id,
+      })
+      )
       )
     )
   }
