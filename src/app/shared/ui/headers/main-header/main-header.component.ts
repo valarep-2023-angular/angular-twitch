5e752d5a44d047bf66c995fe263574b32dc25595
EllVodka <eric.sergueev@gmail.com>
2023-03-29 15:47:43 +0200

    feat(search): make query with search bar

--- src/app/shared/ui/headers/main-header/main-header.component.ts
+++ src/app/shared/ui/headers/main-header/main-header.component.ts
@@ -1,10 +1,45 @@
-import {Component} from '@angular/core';
+import { Component,  OnDestroy, OnInit, Output } from '@angular/core';
+import { Subject, Subscription, tap } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, switchMap } from 'rxjs/operators';
+import { CategoriesService } from 'src/app/shared/data-access/categories/categories.service';
+import { GameDto } from 'src/app/shared/dto/game.dto';
 
 @Component({
   selector: 'app-main-header',
   templateUrl: './main-header.component.html',
   styleUrls: ['./main-header.component.scss']
 })
-export class MainHeaderComponent {
+export class MainHeaderComponent implements OnInit, OnDestroy {
+  debounceTime = 500;
 
+  inputWritten: string = "";
+
+  inputSuscription?: Subscription;
+  inputSubject$ = new Subject<string>();
+
+  categories: GameDto[] = [];
+
+  constructor(private categoriesService: CategoriesService) { }
+
+  ngOnInit() {
+    this.inputSuscription = this.inputSubject$.pipe(
+      filter(input => input.length > 0),
+      debounceTime(this.debounceTime),
+      distinctUntilChanged(),
+      switchMap(input => this.categoriesService.getCategoriesByQueries(input)),
+    ).subscribe(newInput => {
+      this.categories = [...newInput];
+    });
+  }  
+
+  onInput(e: any) {
+    this.inputSubject$.next(e.target.value);
+    console.log("new",this.categories);
+  }
+
+  ngOnDestroy() {
+    if (this.inputSuscription) {
+      this.inputSuscription.unsubscribe();
+    }
+  }
 }
