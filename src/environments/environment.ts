ffaad53da3788aa0cc2240df9d585207d2b74c77
revrard <rudy.evrard@lyreco.com>
2023-04-11 20:48:56 +0200

    ci: remove unused github ci/cd workflow

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 const apiConfig = {
-  clientId: '60tgqfuy4ig9qilavz20n3o2e7afje',
+  clientId: 'efrmnqokv2zxydq5aqco0p3i0tqpg4',
   authUrl: 'https://id.twitch.tv/oauth2/authorize',
-  redirectUrl: 'https://valarep-2023.netlify.app/auth/authorize',
+  redirectUrl: 'http://localhost:4200/auth/authorize',
   baseUrl: 'https://api.twitch.tv/helix',
 };
 
@@ -10,12 +10,12 @@ const oAuthConfig = {
 };
 
 export const environment = {
-  production: true,
+  production: false,
   api: {...apiConfig, ...oAuthConfig},
   twitch: {
-    streamPlayerUrl: 'https://player.twitch.tv/?parent=valarep-2023.netlify.app&channel=',
-    clipPlayerUrl: 'https://clips.twitch.tv/embed?parent=valarep-2023.netlify.app&clip=',
-    videoPlayerUrl: 'https://videos.twitch.tv/embed?parent=valarep-2023.netlify.app&video=',
-    chatUrl: 'https://www.twitch.tv/embed/[USERNAME]/chat?parent=valarep-2023.netlify.app',
+    streamPlayerUrl: 'https://player.twitch.tv/?parent=localhost&channel=',
+    clipPlayerUrl: 'https://clips.twitch.tv/embed?parent=localhost&clip=',
+    videoPlayerUrl: 'https://videos.twitch.tv/embed?parent=localhost&video=',
+    chatUrl: 'https://www.twitch.tv/embed/[USERNAME]/chat?parent=localhost',
   }
 };
