b077362d2b3d641111c42075dc924221d662f12b
revrard <rudy.evrard@lyreco.com>
2023-04-11 21:30:27 +0200

    ci: fix dev and prod builds

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,21 @@
+const apiConfig = {
+  clientId: 'efrmnqokv2zxydq5aqco0p3i0tqpg4',
+  authUrl: 'https://id.twitch.tv/oauth2/authorize',
+  redirectUrl: 'http://localhost:4200/auth/authorize',
+  baseUrl: 'https://api.twitch.tv/helix',
+};
+
+const oAuthConfig = {
+  authLink: `${apiConfig.authUrl}?client_id=${apiConfig.clientId}&response_type=token&redirect_uri=${apiConfig.redirectUrl}`
+};
+
+export const environment = {
+  production: false,
+  api: {...apiConfig, ...oAuthConfig},
+  twitch: {
+    streamPlayerUrl: 'https://player.twitch.tv/?parent=localhost&channel=',
+    clipPlayerUrl: 'https://clips.twitch.tv/embed?parent=localhost&clip=',
+    videoPlayerUrl: 'https://videos.twitch.tv/embed?parent=localhost&video=',
+    chatUrl: 'https://www.twitch.tv/embed/[USERNAME]/chat?parent=localhost',
+  }
+};
