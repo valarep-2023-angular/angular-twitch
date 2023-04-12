02641db7661a55eeaebe86622cd92b9efe9a498b
Julian <juju.leroy47@gmail.com>
2023-04-12 09:52:16 +0200

    fix player prblm*

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,9 +13,9 @@ export const environment = {
   production: true,
   api: {...apiConfig, ...oAuthConfig},
   twitch: {
-    streamPlayerUrl: 'https://player.twitch.tv/?parent=valarep-2023.netlify.app&channel=',
-    clipPlayerUrl: 'https://clips.twitch.tv/embed?parent=valarep-2023.netlify.app&clip=',
-    videoPlayerUrl: 'https://videos.twitch.tv/embed?parent=valarep-2023.netlify.app&video=',
-    chatUrl: 'https://www.twitch.tv/embed/[USERNAME]/chat?parent=valarep-2023.netlify.app',
+    streamPlayerUrl: 'https://player.twitch.tv/?parent=angular-julian.netlify.app&channel=',
+    clipPlayerUrl: 'https://clips.twitch.tv/embed?parent=angular-julian.netlify.app&clip=',
+    videoPlayerUrl: 'https://videos.twitch.tv/embed?parent=angular-julian.netlify.app&video=',
+    chatUrl: 'https://www.twitch.tv/embed/[USERNAME]/chat?parent=angular-julian.netlify.app',
   }
 };
